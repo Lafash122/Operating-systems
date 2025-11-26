dba6ee6d41087112f2f65fd5560ea06640d43836
Lafash122 <120730181+Lafash122@users.noreply.github.com>
2025-11-26 15:57:00 +0700

    Add files via upload
    Добавлено объвление функций и структур

--- 1.6/main.c
+++ 1.6/main.c
@@ -1,3 +1,6 @@
 #define _GNU_SOURCE
+
 #include "mythread.h"
+
+#include <stdio.h>
 
---
+++ 1.6/mythread.h
@@ -0,0 +1,28 @@
+#ifndef MY_THREAD
+#define MY_THREAD
+
+#define _GNU_SOURCE
+
+#include <sys/types.h>
+#include <stddef.h>
+
+typedef struct mythread
+{
+    pid_t tid;
+    void *stack;
+    size_t stack_size;
+
+    void *retval;
+
+    void *(*start_routine)(void *);
+
+    void *arg;
+} mythread_t;
+
+int mythread_create(mythread_t thread, void *(*start_routine)(void *), void *arg);
+
+int mythread_join(mythread_t thread, void **retval);
+
+int mythread_detach(mythread_t thread);
+
+#endif //MY_THREAD
