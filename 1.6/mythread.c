f02724bd7e7e67568b22efc0b7d21835de5bb039
Lafash122 <120730181+Lafash122@users.noreply.github.com>
2025-12-04 00:55:17 +0700

    Add files via upload

--- 1.6/mythread.c
+++ 1.6/mythread.c
@@ -1,3 +1,40 @@
 #define _GNU_SOURCE
+
 #include "mythread.h"
 
+#include <stdatomic.h>
+#include <sys/syscall.h>
+#include <linux/futex.h>
+
+static inline int futex_wait(_Atomic int *uaddr, int value) {
+    return syscall(SYS_futex, (int *) uaddr, FUTEX_WAIT, value, NULL, NULL, 0);
+}
+
+static inline int futex_wake(_Atomic int *uaddr, int value) {
+    return syscall(SYS_futex, (int *) uaddr, FUTEX_WAKE, value, NULL, NULL, 0);
+}
+
+int mythread_join(mythread_t *thread, void **retval) {
+    if (!thread)
+        return -1;
+
+    
+
+    return 0;
+}
+
+int mythread_detach(mythread_t *thread) {
+    if (!thread)
+        return -1;
+
+    
+    
+    return 0;
+}
+
+int mythread_create(mythread_t *thread, void *(*start_routine)(void *), void *arg) {
+    if (!thread || !start_routine)
+        return -1;
+
+    return 0;
+}
--- 1.6/mythread.h
+++ 1.6/mythread.h
@@ -19,10 +19,10 @@ typedef struct mythread
     void *arg;
 } mythread_t;
 
-int mythread_create(mythread_t thread, void *(*start_routine)(void *), void *arg);
+int mythread_create(mythread_t *thread, void *(*start_routine)(void *), void *arg);
 
-int mythread_join(mythread_t thread, void **retval);
+int mythread_join(mythread_t *thread, void **retval);
 
-int mythread_detach(mythread_t thread);
+int mythread_detach(mythread_t *thread);
 
 #endif //MY_THREAD
