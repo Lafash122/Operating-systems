3d08c330b076bc76a832d389a682b106eca8dc28
Lafash122 <120730181+Lafash122@users.noreply.github.com>
2025-12-27 00:21:45 +0700

    Add files via upload

--- proxy/main.c
+++ proxy/main.c
@@ -11,37 +11,83 @@
 #define PROXY_PORT 80
 #define CONN_Q_SIZE 256
 #define BUFFER_SIZE 160 * 1024
-#define METHODS_NUM 128
 #define H_NAME_SIZE 256
-#define H_VAL_SIZE 1024
-
-typedef struct HTTP_HEADER {
-    char h_name[H_NAME_SIZE];
-    char h_value[H_VAL_SIZE];
-} HEADER;
+#define H_VAL_SIZE 2048
 
 int check_meth_line(char *buffer, char *url) {
     char method[16];
-    char version[16];
+    char version[12];
 
-    if (sscanf(buffer, "%15s %2047s %15s", method, url, version) != 3) {
+    if (sscanf(buffer, "%15s %2047s %11s", method, url, version) != 3) {
         printf("Failed to parse HTTP query mettod line");
-        free(buffer);
-
         return 1;
     }
 
     if (strncmp(method, "GET", 3) != 0) {
         printf("Sorry, but only GET method is supported");
-        free(buffer);
-
         return 1;
     }
 
     if (strncmp(version, "HTTP/1.0", 8) != 0) {
         printf("Sorry, but only HTTP/1.0 unput version is supported");
-        free(buffer);
+        return 1;
+    }
 
+    return 0;
+}
+
+int check_headers(char *buffer, char *host) {
+    char *headers = strstr(buffer, "\r\n");
+    if (!headers) {
+        printf("Cannot find headers");
+        return 1;
+    }
+
+    headers += 2;
+    host[0] = '\0';
+    while(1) {
+        if ((headers[0] == '\r') && (headers[1] == '\n'))
+            break;
+
+        char *colon = strchr(headers, ':');
+        if (!colon) {
+            printf("Incorrect structure of header: cannot find colon");
+            return 1;
+        }
+        size_t h_name_len = colon - headers;
+        if ((h_name_len == 0) || (h_name_len >= H_NAME_SIZE)) {
+            printf("Incorrect structure of header: invalid colon location");
+            return 1;
+        }
+
+        char h_name[H_NAME_SIZE];
+        memcpy(h_name, headers, h_name_len);
+        h_name[h_name_len] = '\0';
+
+        char *val_start = colon + 1;
+        while (*val_start == ' ')
+            val_start++;
+        char *val_end = strstr(val_start, "\r\n");
+        if (!val_end) {
+            printf("Incorrect structure of header: cannot find header end");
+            return 1;
+        }
+        size_t h_value_len = val_end - val_start;
+        if ((h_value_len == 0) || (h_value_len >= H_VAL_SIZE)) {
+            printf("Incorrect structure of header: invalid header end location");
+            return 1;
+        }
+
+        if (strcasecmp(h_name, "host") == 0) {
+            memcpy(host, val_start, h_value_len);
+            host[h_value_len] = '\0';
+        }
+
+        headers = val_end + 2;
+    }
+
+    if (host[0] == 0) {
+        printf("Could not find Host header");
         return 1;
     }
 
@@ -52,24 +98,16 @@ void *handle_client(void *arg) {
     int client_sock_fd = *(int *)arg;
     free(arg);
 
-    char *buffer = (char *)calloc(BUFFER_SIZE, sizeof(char));
-    if (!buffer) {
-        perror("Failed to allocate memory for connection");
-        close(client_sock_fd);
-        return NULL;
-    }
-
+    char buffer[BUFFER_SIZE];
     ssize_t readed = read(client_sock_fd, buffer, BUFFER_SIZE - 1);
     if (readed == -1) {
         perror("Failed when read HTTP query");
         close(client_sock_fd);
-        free(buffer);
         return NULL;
     }
     else if (readed == 0) {
         printf("Client sent end of file");
         close(client_sock_fd);
-        free(buffer);
         return NULL;
     }
 
@@ -79,7 +117,13 @@ void *handle_client(void *arg) {
         return NULL;
     }
 
-    
+    char host[H_VAL_SIZE];
+    if (check_headers(buffer, host)) {
+        close(client_sock_fd);
+        return NULL;
+    }
+
+
 
     close(client_sock_fd);
     return NULL;
