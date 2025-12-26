0c477124f324790527d79de3dd3bd7902bb59d68
Lafash122 <120730181+Lafash122@users.noreply.github.com>
2025-12-27 02:15:46 +0700

    Add files via upload

--- proxy/main.c
+++ proxy/main.c
@@ -14,8 +14,9 @@
 #define H_NAME_SIZE 256
 #define H_VAL_SIZE 2048
 
-int check_meth_line(char *buffer, char *url) {
+int check_meth_line(char *buffer) {
     char method[16];
+    char url[2048];
     char version[12];
 
     if (sscanf(buffer, "%15s %2047s %11s", method, url, version) != 3) {
@@ -28,10 +29,14 @@ int check_meth_line(char *buffer, char *url) {
         return 1;
     }
 
-    if (strncmp(version, "HTTP/1.0", 8) != 0) {
-        printf("Sorry, but only HTTP/1.0 unput version is supported");
+    if ((strncmp(version, "HTTP/1.0", 8) != 0) || (strncmp(version, "HTTP/1.1", 8) != 0)) {
+        printf("Sorry, but only HTTP/1.0 or HTTP/1.1 version is supported");
         return 1;
     }
+
+    char *version_pos = strstr(buffer, version);
+    if (version_pos)
+        memcpy(version_pos, "HTTP/1.1", 8);
 
     return 0;
 }
@@ -94,8 +99,40 @@ int check_headers(char *buffer, char *host) {
     return 0;
 }
 
-int resolve_host(char *host, struct sockaddr_in *remote) {
-    
+int resolve_host(const char *host, struct sockaddr_in *remote) {
+    char host_name[H_VAL_SIZE];
+    int port = HTTP_PORT;
+
+    const char *colon = strchr(host, ':');
+    if (colon) {
+        size_t host_len = colon - host;
+        memcpy(host_name, host, host_len);
+        host_name[host_len] = '\0';
+        port = atoi(colon + 1);
+        if (port == 0)
+            port = HTTP_PORT;
+    }
+    else {
+        strncpy(host_name, host, H_VAL_SIZE);
+        host_name[H_VAL_SIZE - 1] = '\0';
+    }
+
+    struct addrinfo info, *res;
+    memset(&info, 0, sizeof(info));
+    info.ai_family = AF_INET;
+    info.ai_socktype = SOCK_STREAM;
+
+    if (getaddrinfo(host_name, NULL, &info, &res) != 0) {
+        perror("Cannot resolve host name");
+        return 1;
+    }
+
+    struct sockaddr_in *ipv4 = (struct sockaddr_in *)res->ai_addr;
+    remote->sin_family = AF_INET;
+    remote->sin_addr = ipv4->sin_addr;
+    remote->sin_port = htons(port);
+
+    freeaddrinfo(res);
 
     return 0;
 }
@@ -117,8 +154,7 @@ void *handle_client(void *arg) {
         return NULL;
     }
 
-    char url[2048];
-    if (check_meth_line(buffer, url)) {
+    if (check_meth_line(buffer)) {
         close(client_sock_fd);
         return NULL;
     }
@@ -131,13 +167,61 @@ void *handle_client(void *arg) {
 
     struct sockaddr_in remote_addr;
     memset(&remote_addr, 0, sizeof(remote_addr));
-    remote_addr.sin_family = AF_INET;
     if (resolve_host(host, &remote_addr)) {
         close(client_sock_fd);
         return NULL;
     }
 
+    int remote_sock_fd;
+    if ((remote_sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Cannot create remote socket");
+        close(client_sock_fd);
+        return 1;
+    }
+
+    if (connect(remote_sock_fd, (struct sockaddr *)&remote_addr, sizeof(remote_addr)) < 0) {
+        perror("Cannot connect to the remote");
+        close(client_sock_fd);
+        close(remote_sock_fd);
+        return NULL;
+    }
+
+    ssize_t sent = 0, to_send = readed;
+    while (sent < to_send) {
+        ssize_t send = write(remote_sock_fd, buffer + sent, to_send - sent);
+        if (send <= 0) {
+            perror("Failed write to remote from client");
+            close(client_sock_fd);
+            close(remote_sock_fd);
+            return NULL;
+        }
+
+        sent += send;
+    }
+
+    ssize_t remote_read;
+    while ((remote_read = read(remote_sock_fd, buffer, BUFFER_SIZE)) > 0) {
+        ssize_t client_sent = 0;
+        while (sent < remote_read) {
+            ssize_t send = write(client_sock_fd, buffer + client_sent, remote_read - client_sent);
+            if (send <= 0) {
+                perror("Failed write to client from remote");
+                close(client_sock_fd);
+                close(remote_sock_fd);
+                return NULL;
+            }
+            client_sent += send;
+        }
+    }
+
+    if (remote_read == -1)
+        perror("Failed when read from remote");
+    else if (remote_read == 0)
+        printf("Remote sent end of file");
+
     close(client_sock_fd);
+    close(remote_sock_fd);
+
     return NULL;
 }
 
